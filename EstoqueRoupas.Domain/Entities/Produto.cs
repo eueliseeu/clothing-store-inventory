a0733759bbef284f3436e37b405eda8f4e54db23
Eliseu Silva <eueliseeu@gmail.com>
2026-09-22 12:58:31 -0300

    feat: criando as entidades do produto.

---
+++ EstoqueRoupas.Domain/Entities/Produto.cs
@@ -0,0 +1,12 @@
+namespace EstoqueRoupas.Domain.Entities;
+
+public class Produto
+{
+    public int Id { get; set; } 
+    public string Nome { get; set; } = string.Empty;
+    public string? Descricao { get; set; }
+    public string Sku { get; set; } = string.Empty;
+    public decimal Preco { get; set; }
+    public int QuantidadeEstoque { get; set; }
+    public DateTime DataCadastro { get; set; } = DateTime.UtcNow;
+}
