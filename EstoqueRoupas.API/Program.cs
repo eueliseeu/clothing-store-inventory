2a25c1595e4d2e23ac1c58140f6a2981322a53ca
Eliseu Silva <eueliseeu@gmail.com>
2026-09-22 13:01:10 -0300

    feat: adicionar registro do serviço IProdutoService no contêiner de injeção de dependência

--- EstoqueRoupas.API/Program.cs
+++ EstoqueRoupas.API/Program.cs
@@ -1,10 +1,14 @@
+using EstoqueRoupas.Domain.Interfaces;
 using EstoqueRoupas.Infrastructure.Data;
+using EstoqueRoupas.Infrastructure.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IProdutoService, ProdutoService>();
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
